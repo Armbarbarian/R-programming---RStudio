c817bdb53d34c3ee01bb7cc4057a84316d72ddd3
Armbarbarian <daniel_goodall@outlook.com>
2020-04-25 17:03:30 +0100

    basics of reading in large tables adding the UKdata.txt file

--- reading_tabular_data.R
+++ reading_tabular_data.R
@@ -12,3 +12,20 @@ x
 # ------------Arguments----------------
 # file, headerr, sep, colClasses, nrows, comment.char, skip, stringsAsFactors
 
+
+# -------------Large tables-----------------------------
+# help page for read.table()
+# help page for read.csv()
+# RAM is used up for the dataset you are reading in.
+# You can specify the file columns, classes and rows to R doesn't figure it out
+  # this saves time and RAM
+
+
+
+
+
+
+
+
+
+
