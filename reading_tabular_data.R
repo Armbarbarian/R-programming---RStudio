9d3e27c66856011802fded7098f1b48a7012a612
Armbarbarian <daniel_goodall@outlook.com>
2020-04-25 16:54:12 +0100

    read csv and arguments etc and Uk data

---
+++ reading_tabular_data.R
@@ -0,0 +1,14 @@
+# read.table()
+# read.csv()
+# readLines() - reads text file
+# source() - reads R code
+# dget - rR code
+# load() - reading in workspaces
+x <- read.csv('UKdata.csv')
+x
+# write.table()
+# writeLines()
+
+# ------------Arguments----------------
+# file, headerr, sep, colClasses, nrows, comment.char, skip, stringsAsFactors
+
